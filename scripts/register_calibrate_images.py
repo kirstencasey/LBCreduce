3f95aa4b25d80dbb486736911c93abdb6ed10596
Kirsten Casey <osu10713@pitzer-rw02.ten.osc.edu>
2024-02-14 13:58:16 -0500

    Updated README, minor improvements

--- scripts/register_calibrate_images.py
+++ scripts/register_calibrate_images.py
@@ -13,16 +13,16 @@ if __name__ == '__main__':
     parser.add_argument('--make-plots', action='store_true')
     parser.add_argument('--log-level', default='info', help='log level (debug, info, warn, error, critical)')
     parser.add_argument('--log-fn', default=None, help='log file name')
-    parser.add_argument('--config_fn', type=str, default=default_config, help='path of the .yml config file ')
+    parser.add_argument('--config', type=str, default=default_config, help='path of the .yml config file ')
     args = parser.parse_args()
     lbcred.utils.setup_logger(args.log_level, args.log_fn)
 
     if args.register_images:
         # Register r-band images
-        config, sky_pos, src = lbcred.register_images(tmp_path=args.tmp_path, bandpass='R', ref_cat=args.ref_cat, index_path=args.index_path, make_plots=args.make_plots, config_fn=args.config_fn)
+        config, sky_pos, src = lbcred.register_images(tmp_path=args.tmp_path, bandpass='R', ref_cat=args.ref_cat, index_path=args.index_path, make_plots=args.make_plots, config_fn=args.config)
 
         # Register b-band images
-        config, sky_pos, src = lbcred.register_images(tmp_path=args.tmp_path, bandpass='B', ref_cat=args.ref_cat, index_path=args.index_path, make_plots=args.make_plots, config_fn=args.config_fn)
+        config, sky_pos, src = lbcred.register_images(tmp_path=args.tmp_path, bandpass='B', ref_cat=args.ref_cat, index_path=args.index_path, make_plots=args.make_plots, config_fn=args.config)
 
         config['image_dir'] = config['out_dir']
         if config['subtract_background']: config['glob_select'] = 'backsub_' + config['glob_select'].replace('.fits','_reg.fits')
@@ -31,6 +31,6 @@ if __name__ == '__main__':
     # Calibrate images
     if args.calibrate_images:
         if args.register_images: config = config
-        else : config = args.config_fn
+        else : config = args.config
 
         lbcred.calibrate_images(config=config)
